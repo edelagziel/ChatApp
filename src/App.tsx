ddf3023c187b4842d8a996812aefed5826db4d28
Eden Lagziel <edenlagziel63@gmail.com>
2026-04-08 16:18:38 +0300

    fix route name in App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,10 @@ import {ProtectedRoutes} from "./routes/ProtectedRoutes"
 import {LoginPage} from "./features/auth/loginPage/loginPage"
 import { Register } from "./features/auth/registerPage/RegisterPage";
 import Box from '@mui/material/Box';
-import {MsgPage} from "./features/msg/msgPage"
+import {MsgPage} from "./features/msg/MsgPage"
+
+
+
 export function App()
 {
   return(
